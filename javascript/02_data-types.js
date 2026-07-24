cd541c8d07d1bcc8cd233674d51b344ade1ec673
Jayati Mahato <24jayatimahato@gmail.com>
2026-07-24 10:02:09 +0530

    I learned about data types, and how to check the type of datatypes.

---
+++ javascript/02_data-types.js
@@ -0,0 +1,23 @@
+"use strict"; // all code treated as new version
+
+// alert(yo!) not applicable in nodejs
+
+let the_age = 56
+let the_name = "Jessica"
+let loggedin = false
+console.table([the_age, the_name, loggedin])
+
+
+//number
+//bitInt
+//string
+//symbol
+
+//object
+//null
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof Int16Array);
+console.log(typeof Number);
+
+
