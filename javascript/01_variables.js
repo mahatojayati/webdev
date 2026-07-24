06c65e21a3735f5a39daa767f2b22aefc6359878
Jayati Mahato <24jayatimahato@gmail.com>
2026-07-24 09:25:22 +0530

    eh, variables with no values are undefined.

--- javascript/01_variables.js
+++ javascript/01_variables.js
@@ -3,8 +3,15 @@ const account_id = 1144;
 let account_email = "slay101@gmail.com";
 var account_password = "secret@1234"
 account_city = "Delhi"
+let account_state;
 
-
-console.table([account_id, account_email, account_password, account_city]);
+/*
+don't use var, cause scope issue.
+*/
+console.table([account_id, 
+    account_email, 
+    account_password, 
+    account_city,
+account_state]);
 
 
