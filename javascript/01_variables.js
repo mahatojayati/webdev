20d78e841e4f37e2169208d9c6b5272b10b73fe0
Jayati Mahato <24jayatimahato@gmail.com>
2026-07-24 09:18:26 +0530

    i learned how to print multiple variables using console.table.

---
+++ javascript/01_variables.js
@@ -0,0 +1,10 @@
+// to get a user to signup and creat an account
+const account_id = 1144;
+let account_email = "slay101@gmail.com";
+var account_password = "secret@1234"
+account_city = "Delhi"
+
+
+console.table([account_id, account_email, account_password, account_city]);
+
+
