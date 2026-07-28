efeb4edcee0e35e8756d1dfc3a117a210b68bfcb
Jayati Mahato <24jayatimahato@gmail.com>
2026-07-28 20:26:11 +0530

    discovery  - no matter the is 2 or -1, it's true, for 0 it's always false.

--- javascript/03_conversion-ops.js
+++ javascript/03_conversion-ops.js
@@ -8,3 +8,10 @@ let valueinnumber  = Number(score);
 
 console.log( typeof valueinnumber);
 console.log(valueinnumber);
+
+// "33" - 33
+// "33qwerty" - NaN
+// "1 or 0" - true/false
+let value  = 0;
+console.log(Boolean(value));
+
