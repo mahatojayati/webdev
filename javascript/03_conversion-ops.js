bf9e666fe1aa7b628a9fbfa89bdee2fbb47a6018
Jayati Mahato <24jayatimahato@gmail.com>
2026-07-28 20:20:58 +0530

    learning about type coversions and messy-ish JS.

---
+++ javascript/03_conversion-ops.js
@@ -0,0 +1,10 @@
+"use strict"
+let score = "33" ;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber  = Number(score);
+
+console.log( typeof valueinnumber);
+console.log(valueinnumber);
