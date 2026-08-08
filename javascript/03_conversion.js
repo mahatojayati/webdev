1f2ec855710ea42dbde627077bec4f1041e66bf9
Jayati Mahato <24jayatimahato@gmail.com>
2026-08-08 09:57:44 +0530

    experimenting with data types.

---
+++ javascript/03_conversion.js
@@ -0,0 +1,40 @@
+"use strict"
+let score
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber  = Number(score);
+
+console.log( typeof valueinnumber);
+console.log(valueinnumber);
+
+// "33" - 33
+// "33qwerty" - NaN
+// "1 or 0" - true/false
+let value  = 0;
+console.log(Boolean(value));
+
+// ' ' - false
+// -1 - true
+// 0 - false
+'beesting' - true
+
+// check negation of values
+let negativevalue  = 5;
+console.log(-negativevalue);
+
+//str operations
+let firstname = "Jayati";
+let lastname = " Mahato";
+let fullname = firstname+lastname;
+console.log(fullname);
+
+console.log(1 + "2");
+console.log("2" + 1);
+console.log(2 + 2 + "1");
+
+
+
+
+
