c686a30c9008819a24fd9e26a5a386f1b277e818
Jayati Mahato <24jayatimahato@gmail.com>
2026-08-18 22:58:27 +0530

    learned comparison operators.

---
+++ javascript/comparison.js
@@ -0,0 +1,16 @@
+"use strict"
+
+//comparison operators
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+//comparison converts null to a number, treating as 0.
+
+console.log(null == 0)
+console.log(null != 0)
+console.log(null < 0)
+console.log(null > 0)
+console.log(null)
+
+
