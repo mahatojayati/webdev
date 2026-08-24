d1df2112b7bae17a2cb2f11dacef461ea7f4da8e
Jayati Mahato <24jayatimahato@gmail.com>
2026-08-24 21:31:29 +0530

    used switch conditionals to print anime names in a list.

---
+++ javascript/conditionals.js
@@ -0,0 +1,25 @@
+const AnimeList = ["Dragon Ball Z ","Naruto","One Piece","Bleach"];
+let userchoice = 5;
+
+switch(userchoice)
+{
+    case 1:
+        console.log(AnimeList[0]);
+        break;
+    case 2:
+        console.log(AnimeList[1]);
+        break;
+    case 3:
+        console.log(AnimeList[2]);
+        break;
+    case 4:
+        console.log(AnimeList[3]);
+        break;
+        default:
+        console.log("Watch Anime");
+        
+}
+
+
+            
+        
