0987ab1eb9badec51e2c4f2d545c05c826444f14
Jayati Mahato <24jayatimahato@gmail.com>
2026-08-31 22:03:22 +0530

    learned about arrowfunction and it's variation.

---
+++ javascript/arrowfunction.js
@@ -0,0 +1,19 @@
+// arrow functions
+
+// syntax
+
+const sayHello = () =>{
+    console.log("Heyy.");
+
+};
+sayHello();
+
+// const addnum = (a,b) =>{
+//     return a+b;
+// };
+
+const addnum = (a,b) => a+b;
+
+console.log(addnum(2,3));
+
+
