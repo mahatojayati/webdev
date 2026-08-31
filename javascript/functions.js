cb379ac4480132b53c6951e3d0539d1833677000
Jayati Mahato <24jayatimahato@gmail.com>
2026-08-31 11:24:25 +0530

    used arguments operators

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,25 +1,26 @@
-// "use strict"
-// function add(number1, number2) {
-//      console.log(number1+number2);
-// }
+"use strict"
+function add(number1, number2) {
+     console.log(number1+number2);
+}
 
-// add(12,34.34)
+add(12,34.34)
 
-// function multiply(number1, number2) {
-//     return number1*number2;
-// }
-// let result = multiply(12, 56)
-// console.log("User the result is", result);
+function multiply(number1, number2) {
+    return number1*number2;
+}
+let multiplyresult = multiply(12, 56)
+console.log("User the result is", multiplyresult);
 
 
-function sumofallnumbers() {
-    let ans = 0;
+
+function addnumbers() {
+    let result  = 0
     for(let i = 0; i<arguments.length; i++)
     {
-        ans = ans+arguments[i];
+        result = result + arguments[i]
     }
+    return result
 }
- let result = sumofallnumbers(10,23,55,45)
- console.log(result);
-    
-    
+let addresult = addnumbers(10,23,4,6,3,75,4)
+console.log(result);
+
