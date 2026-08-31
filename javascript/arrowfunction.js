b165a884819f1ba5f5c27f4dd68680c412f320ab
Jayati Mahato <24jayatimahato@gmail.com>
2026-08-31 22:24:33 +0530

    using argument keyword.

--- javascript/arrowfunction.js
+++ javascript/arrowfunction.js
@@ -12,8 +12,15 @@ sayHello();
 //     return a+b;
 // };
 
-const addnum = (a,b) => a+b;
+const addnum = (a,b) => a+b; // one liner
 
 console.log(addnum(2,3));
 
+// argument keyword
 
+function multiplynumber() {
+    console.log(arguments);
+    
+}
+
+multiplynumber(10,23,45);
